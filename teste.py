54352fe04922131b336c0ca0ed10725ae8446129
Diefferson Siqueira <60404007+DieffersonS@users.noreply.github.com>
2026-08-26 19:12:06 +0000

    Subindo o exercicio da aula 133 e a aula 128

--- teste.py
+++ teste.py
@@ -1,21 +1,5 @@
-perguntas = [
-    {
-        'Pergunta':'Quanto é 2 + 2?',
-        'Opções':['1','2','4','5'],
-        'Resposta':'4',
-    },
-    {
-        'Pergunta':'Quanto é 5 x 5?',
-        'Opções':['25','55','10','51'],
-        'Resposta':'25',
-    },
-    {
-        'Pergunta':'Quanto é 10 ÷ 2?',
-        'Opções':['4','5','2','1'],
-        'Resposta':'5',
-    },
-]
+s1 = {1, 2, 3}
 
-for i in perguntas:
-    for y in i['Opções']:
-        print(y)
+s1.discard(2)
+
+print(s1)
